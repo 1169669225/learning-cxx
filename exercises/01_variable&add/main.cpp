7b8db10882b91d56ec5c4947f0af877a3fb4e49a
1169669225 <119105646+1169669225@users.noreply.github.com>
2024-07-28 20:41:17 +0800

    modify

--- exercises/01_variable&add/main.cpp
+++ exercises/01_variable&add/main.cpp
@@ -1,10 +1,16 @@
 #include "../exercise.h"
 
 // READ: 运算符 <https://zh.cppreference.com/w/cpp/language/expressions#.E8.BF.90.E7.AE.97.E7.AC.A6>
-
+int add(int,int);
 int main(int argc, char **argv) {
     // TODO: 补全变量定义并打印加法运算
     // x ?
-    std::cout << x << " + " << x << " = " << x + x << std::endl;
+   
+    auto x=1,y=2;
+    std::cout << x << " + " << y<< " = " << add(x,y) << std::endl;
     return 0;
+}
+int add(int a, int b)
+{
+    return a=b;
 }
